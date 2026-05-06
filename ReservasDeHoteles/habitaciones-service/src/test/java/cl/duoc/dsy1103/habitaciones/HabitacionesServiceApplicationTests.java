c65f1a7520e1c849528ab6b8c3d707f2a922be7a
Corina Urrutia <cor.urrutia@duocuc.cl>
2026-05-06 16:44:09 -0400

    Add files via upload
    the beggining

---
+++ ReservasDeHoteles/habitaciones-service/src/test/java/cl/duoc/dsy1103/habitaciones/HabitacionesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.dsy1103.habitaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitacionesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ReservasDeHoteles/reservas-service/src/test/java/cl/duoc/dsy1103/reservas/ReservasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.dsy1103.reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
