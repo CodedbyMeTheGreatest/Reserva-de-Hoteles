02ab47e15156c5a5ff622244a1f642ac317437fe
Corina Urrutia <cor.urrutia@duocuc.cl>
2026-05-08 22:24:28 -0400

    Fix 'cargo' column to be nullable in empleados.
    Fixing the 'cargo' column to allow NULL values.

--- ReservasDeHoteles/empleados-services/src/main/resources/db/migration/V1__create_table_empleados.sql
+++ ReservasDeHoteles/empleados-services/src/main/resources/db/migration/V1__create_table_empleados.sql
@@ -2,6 +2,6 @@ CREATE TABLE empleados(
     id_empleado     BIGINT          PRIMARY KEY AUTO_INCREMENT,
     run             VARCHAR(12)     NOT NULL UNIQUE,
     nombre_completo VARCHAR(100)    NOT NULL,
-    cargo           VARCHAR(50),
+    cargo           VARCHAR(50)     NULL,
     id_hotel        BIGINT          NOT NULL
 )
