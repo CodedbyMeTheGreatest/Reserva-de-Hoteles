0299d4b84d7fda48658c8c79151afca829d36bb6
JeanWLK <je.lefiman@duocuc.cl>
2026-05-09 17:04:10 -0400

    Add files via upload

---
+++ ReservasDeHoteles/pagos-service/src/test/java/cl/duoc/dsy1103/pagos/PagosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.dsy1103.pagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
