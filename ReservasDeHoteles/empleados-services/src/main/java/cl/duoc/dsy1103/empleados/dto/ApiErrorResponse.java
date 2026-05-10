81db452df6eab5d58b870f6f2ea848cd78ee7589
Corina Urrutia <cor.urrutia@duocuc.cl>
2026-05-10 14:17:45 -0400

    feat(check-in): arregla tipo de variable en ApiErrorResponse

--- ReservasDeHoteles/empleados-services/src/main/java/cl/duoc/dsy1103/empleados/dto/ApiErrorResponse.java
+++ ReservasDeHoteles/empleados-services/src/main/java/cl/duoc/dsy1103/empleados/dto/ApiErrorResponse.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -17,5 +18,5 @@ public class ApiErrorResponse {
     private String error;
     private String message;
     private String path;
-    private String errors;
+    private List<String> errors;
 }
