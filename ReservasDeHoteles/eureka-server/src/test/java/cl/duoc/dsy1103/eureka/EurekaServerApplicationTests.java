83ecb3099132299ead838406539b67cdba120d7d
Corina Urrutia <cor.urrutia@duocuc.cl>
2026-06-19 21:13:56 -0400

    feat: añadiendo microservicios de gateway y eureka

---
+++ ReservasDeHoteles/eureka-server/src/test/java/cl/duoc/dsy1103/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.dsy1103.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
