0777f71e75fb1bf64615a24012d32886c8e7756f
Corina Urrutia <cor.urrutia@duocuc.cl>
2026-06-22 18:12:43 -0400

    fix: arreglando milagros

--- ReservasDeHoteles/huespedes-service/src/main/java/cl/duoc/dsy1103/huespedes/HuespedesServiceApplication.java
+++ ReservasDeHoteles/huespedes-service/src/main/java/cl/duoc/dsy1103/huespedes/HuespedesServiceApplication.java
@@ -2,8 +2,10 @@ package cl.duoc.dsy1103.huespedes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HuespedesServiceApplication {
 
 	public static void main(String[] args) {
